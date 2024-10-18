b70850d8ea0b3b9048588fc0264a141bae647e86
dzhuleikhan <dzhuleikhan@gmail.com>
2024-10-18 20:30:29 +0700

    Changed Geo API

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import "./style.css";
 import "intl-tel-input/build/css/intlTelInput.css";
 
-import "./modules/getLocation";
+import "./modules/geoLocation";
 import "./modules/animations";
 import "./modules/spinning";
 import "./modules/phoneRotate";
 import "./modules/formModal";
 import "./modules/formValidation";
 import "./modules/modalCurrency";
-import "./modules/getLocation";
