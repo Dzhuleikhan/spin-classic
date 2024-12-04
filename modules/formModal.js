a9565d6702ccd97a2bf1726be983094dc0d50e8c
dzhuleikhan <dzhuleikhan@gmail.com>
2024-12-04 20:01:39 +0300

    Spin TR Done

--- modules/formModal.js
+++ modules/formModal.js
@@ -1,32 +1,3 @@
-import intlTelInput from "intl-tel-input/intlTelInputWithUtils";
-
-const input = document.querySelector(".phone-input");
-
-const geoIpLookup = (success, failure) => {
-  const cachedData = localStorage.getItem("geoIpData");
-  if (cachedData) {
-    success(JSON.parse(cachedData).country);
-  } else {
-    fetch("https://ipinfo.io/json?token=fcd65e5fcfdda1")
-      .then((res) => res.json())
-      .then((data) => {
-        localStorage.setItem("geoIpData", JSON.stringify(data));
-        success(data.country);
-      })
-      .catch(() => {
-        failure();
-      });
-  }
-};
-
-export const iti = intlTelInput(input, {
-  initialCountry: "auto",
-  separateDialCode: true,
-  useFullscreenPopup: false,
-  autoPlaceholder: "polite",
-  geoIpLookup: geoIpLookup,
-});
-
 const modalOpenBtn = document.querySelectorAll(".modal-open-btn");
 const formOverlay = document.querySelector(".form-overlay");
 const formModalClosebtn = document.querySelector(".form-modal-close-btn");
