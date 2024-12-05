dfafea2cacdf38055e7981d6bc1dae2cbffd2222
dzhuleikhan <dzhuleikhan@gmail.com>
2024-12-05 22:04:53 +0300

    UZ updated bonus ID numbers

--- modules/fetchingDomain.js
+++ modules/fetchingDomain.js
@@ -5,3 +5,28 @@ export const fetchDomain = async () => {
 };
 
 export const newDomain = await fetchDomain();
+
+function updatingBonusValueNumbers() {
+  const dropd = document.querySelectorAll(".form-bonus-dropdown");
+  dropd.forEach((drop) => {
+    if (drop) {
+      const links = drop.querySelectorAll("li");
+      links[0].setAttribute("data-bonus-id", "120");
+      links[1].setAttribute("data-bonus-id", "122");
+      links[2].setAttribute("data-bonus-id", "121");
+    }
+  });
+  const initialBonusValueInput = document.querySelectorAll(
+    ".auth-form-bonus input",
+  );
+  initialBonusValueInput.forEach((input) => {
+    input.value = 120;
+  });
+
+  const formSocials = document.querySelectorAll(".form-modal-socials");
+  formSocials.forEach((form) => {
+    let input = form.querySelector(".bonus-input");
+    input.setAttribute("data-bonus", 120);
+  });
+}
+updatingBonusValueNumbers();
