f483b1f4f5ffb5c178f4531061a961630ee15530
dzhuleikhan <dzhuleikhan@gmail.com>
2024-12-19 16:26:36 +0300

    Adde phone tab param

--- modules/params.js
+++ modules/params.js
@@ -15,6 +15,19 @@ export function setUrlParameter(key, value) {
   window.history.pushState({ path: url.href }, "", url.href);
 }
 
+function showSocialsMethod(method) {
+  document.querySelectorAll(".form-type-btn").forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  document.querySelectorAll(".form-group").forEach((inputs) => {
+    inputs.classList.remove("active");
+  });
+  document
+    .querySelector(`.form-type-buttons div[data-tab='${method}']`)
+    .classList.add("active");
+  document.querySelector(`.form-group-${method}`).classList.add("active");
+}
+
 // Ensure 'modal' parameter is set to 'sign-up-2-steps' by default
 let modal = getUrlParameter("modal") || "socials";
 setUrlParameter("modal", modal);
@@ -22,6 +35,18 @@ setUrlParameter("modal", modal);
 // Ensure 'method' parameter is set to 'google,fb' by default
 let method = getUrlParameter("method") || "google,fb";
 setUrlParameter("method", method);
+
+if (modal === "socials") {
+  if (!getUrlParameter("method-type")) {
+    addUrlParameter("method-type", "email");
+  }
+  const method = getUrlParameter("method-type");
+  if (method === "phone") {
+    showSocialsMethod("phone");
+  } else {
+    showSocialsMethod("email");
+  }
+}
 
 // Ensure 'mode' parameter is set to 'fixed' by default
 let mode = getUrlParameter("mode") || "fixed";
@@ -32,18 +57,16 @@ let bonus = getUrlParameter("bonus");
 const bonusWrapper = document.querySelector(".bonus-wrapper");
 const bonusWrapperInput = document.querySelector(".bonus-input");
 
-// if (bonus) {
-//   bonusWrapper.classList.remove("hidden");
-//   bonusWrapperInput.setAttribute("data-bonus", bonus);
-// } else {
-//   console.log("No Bonuse");
-//   bonusWrapper.classList.add("hidden");
-//   bonusWrapperInput.setAttribute("data-bonus", "");
-// }
-
 // Function to update the URL with a new parameter and value
 export function updateUrl(key, value) {
   const url = new URL(window.location.href);
+  url.searchParams.set(key, value);
+  window.history.pushState({ path: url.href }, "", url.href);
+}
+
+// Function to add a parameter to the URL
+function addUrlParameter(key, value) {
+  var url = new URL(window.location.href);
   url.searchParams.set(key, value);
   window.history.pushState({ path: url.href }, "", url.href);
 }
