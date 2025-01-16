339bb791ae7bc78a124a74bcc49a6f9a084311de
dzhuleikhan <dzhuleikhan@gmail.com>
2025-01-16 14:00:30 +0300

    1

--- modules/formValidation.js
+++ modules/formValidation.js
@@ -358,7 +358,7 @@ if (mainForm) {
     formData.password = password.value;
     formData.currency = currency.value;
     formData.bonus = bonus;
-    formData.lang = lang;
+    formData.lang = localStorage.getItem("preferredLanguage");
 
     let code = iti.getSelectedCountryData().dialCode;
     let phoneNumber = phone.value.trim();
