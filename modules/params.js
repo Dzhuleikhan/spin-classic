838f12d2c80a7bbcc98502f11cb256371181f042
dzhuleikhan <dzhuleikhan@gmail.com>
2025-01-16 13:55:24 +0300

    Last changes

--- modules/params.js
+++ modules/params.js
@@ -15,9 +15,34 @@ export function setUrlParameter(key, value) {
   window.history.pushState({ path: url.href }, "", url.href);
 }
 
+function showSocialsMethod(method) {
+  document.querySelectorAll(".form-type-btn").forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  document.querySelectorAll(".form-group").forEach((inputs) => {
+    inputs.classList.remove("active");
+  });
+  document
+    .querySelector(`.form-type-buttons div[data-tab='${method}']`)
+    .classList.add("active");
+  document.querySelector(`.form-group-${method}`).classList.add("active");
+}
+
 // Ensure 'modal' parameter is set to 'sign-up-2-steps' by default
 let modal = getUrlParameter("modal") || "socials";
 setUrlParameter("modal", modal);
+
+if (modal === "socials") {
+  if (!getUrlParameter("method-type")) {
+    addUrlParameter("method-type", "email");
+  }
+  const method = getUrlParameter("method-type");
+  if (method === "phone") {
+    showSocialsMethod("phone");
+  } else {
+    showSocialsMethod("email");
+  }
+}
 
 // Ensure 'method' parameter is set to 'google,fb' by default
 let method = getUrlParameter("method") || "google,fb";
@@ -44,6 +69,13 @@ const bonusWrapperInput = document.querySelector(".bonus-input");
 // Function to update the URL with a new parameter and value
 export function updateUrl(key, value) {
   const url = new URL(window.location.href);
+  url.searchParams.set(key, value);
+  window.history.pushState({ path: url.href }, "", url.href);
+}
+
+// Function to add a parameter to the URL
+function addUrlParameter(key, value) {
+  var url = new URL(window.location.href);
   url.searchParams.set(key, value);
   window.history.pushState({ path: url.href }, "", url.href);
 }
