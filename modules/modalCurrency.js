e2719b16b84edeb1339d981a602447a23af5ef55
dzhuleikhan <dzhuleikhan@gmail.com>
2025-01-28 15:23:59 +0300

    Removed RUB

--- modules/modalCurrency.js
+++ modules/modalCurrency.js
@@ -54,7 +54,12 @@ function setCurrency(abbr, name, icon) {
 async function settingModalCurrency() {
   try {
     let locationData = await getLocation();
-    const countryInput = locationData.countryCode;
+    let countryInput = locationData.countryCode;
+
+    if (countryInput === "RU") {
+      countryInput = "US";
+    }
+
     const currencyAbbr = getCountryCurrencyABBR(countryInput);
     const currencyFullName = getCountryCurrencyFullName(countryInput);
     const currencyIcon = getCountryCurrencyIcon(countryInput);
