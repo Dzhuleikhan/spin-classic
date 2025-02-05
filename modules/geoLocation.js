956ca858d9c164630fbe8068ed0e729cab22f9bb
dzhuleikhan <dzhuleikhan@gmail.com>
2025-02-05 10:43:50 +0300

    2

--- modules/geoLocation.js
+++ modules/geoLocation.js
@@ -4,7 +4,7 @@ export async function getLocation() {
   let url = "https://cdndigitaloceanspaces.cloud/geoip";
   let response = await fetch(url);
   let data = await response.json();
-  localStorage.setItem("preferredLanguage", "ca");
+  localStorage.setItem("preferredLanguage", "en");
   hidePreloader();
 
   return data;
