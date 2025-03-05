32eb9648fbb84d54844f5202ea7965b9c7ac847c
dzhuleikhan <dzhuleikhan@gmail.com>
2025-03-05 20:30:13 +0530

    Changed rotator CA 4

--- modules/geoLocation.js
+++ modules/geoLocation.js
@@ -1,7 +1,8 @@
 import { hidePreloader } from "./phoneRotate";
 
 export async function getLocation() {
-  let url = "https://cdndigitaloceanspaces.cloud/geoip";
+  let url =
+    "https://apiip.net/api/check?accessKey=0439ba6e-6092-46c2-9aeb-8662065bc43c";
   let response = await fetch(url);
   let data = await response.json();
   localStorage.setItem("preferredLanguage", "en");
