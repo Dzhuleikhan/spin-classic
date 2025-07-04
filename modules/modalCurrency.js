a9c6bca9743fb8967a3f4b593ae226c19b0fdec5
dzhuleikhan <dzhuleikhan@gmail.com>
2025-07-04 19:12:50 +0400

    Excluded "RU", "MX", "CL", "CO", "TH", "ID" and Removed IDR

--- modules/modalCurrency.js
+++ modules/modalCurrency.js
@@ -56,13 +56,10 @@ async function settingModalCurrency() {
     let locationData = await getLocation();
     let countryInput = locationData.countryCode;
 
-    if (
-      countryInput === "RU" ||
-      countryInput === "MX" ||
-      countryInput === "CO" ||
-      countryInput === "CL"
-    ) {
-      countryInput = "US";
+    const excludedCountries = ["RU", "MX", "CL", "CO", "TH", "ID"];
+
+    if (excludedCountries.includes(countryInput)) {
+      countryInput = "KZ";
     }
 
     const currencyAbbr = getCountryCurrencyABBR(countryInput);
