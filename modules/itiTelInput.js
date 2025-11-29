be13aaf45d1a9e45f245adf45e27c3a1501968f3
dzhuleikhan <dzhuleikhan@gmail.com>
2025-11-30 03:32:49 +0400

    new API

--- modules/itiTelInput.js
+++ modules/itiTelInput.js
@@ -1,23 +1,13 @@
 import intlTelInput from "intl-tel-input/intlTelInputWithUtils";
+import { geoData } from "./geoLocation";
 
 const socialsPhoneInput = document.querySelector(".socials-phone-input");
 
 const geoIpLookup = (success, failure) => {
-  const cachedData = localStorage.getItem("geoIpData");
-  if (cachedData) {
-    success(JSON.parse(cachedData).countryCode);
+  if (geoData && geoData.countryCode) {
+    success(geoData.countryCode);
   } else {
-    fetch(
-      "https://apiip.net/api/check?accessKey=0439ba6e-6092-46c2-9aeb-8662065bc43c",
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        localStorage.setItem("geoIpData", JSON.stringify(data));
-        success(data.countryCode);
-      })
-      .catch(() => {
-        failure();
-      });
+    success("PL");
   }
 };
 
@@ -26,5 +16,5 @@ export const iti = intlTelInput(socialsPhoneInput, {
   separateDialCode: true,
   useFullscreenPopup: false,
   autoPlaceholder: "polite",
-  geoIpLookup: geoIpLookup,
+  geoIpLookup,
 });
